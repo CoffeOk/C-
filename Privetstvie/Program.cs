0fe04c88249c54121e496ee2c43c9c313feb307e
Wall-e <you@mail.com>
2023-09-17 01:09:51 +0300

    Osoboe privetstvie

---
+++ Privetstvie/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Vvedite name polzovatelja: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Zdravia zelay tovarish Masha");
+}
+else
+{
+    Console.Write("Hallo, ");
+    Console.WriteLine(username);
+}
